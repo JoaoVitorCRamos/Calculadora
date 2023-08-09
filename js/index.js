9ae7701e57b604abd876ec8a6c7cf22d7eadcb04
Joao Vitor Costa <139905189+JoaoVitorCRamos@users.noreply.github.com>
2023-08-09 04:29:29 -0300

    Fiz alterações usando modularização

---
+++ js/index.js
@@ -0,0 +1,14 @@
+import calculate from "./calculate.js"
+import copyToClipboard from "./copyToClipboard.js"
+import { handleButtonPress, handleClear, handleTyping } from "./keyHandlers.js"
+import themeSwitcher from "./themeSwitcher.js"
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", handleButtonPress)
+})
+
+document.getElementById("clear").addEventListener("click", handleClear)
+input.addEventListener("keydown", handleTyping)
+document.getElementById("equal").addEventListener("click", calculate)
+document.getElementById("copyToClipboard").addEventListener("click", copyToClipboard)
+document.getElementById("themeSwitcher").addEventListener("click", themeSwitcher)
